fd9a7abe4c30ffa5818be6507cf52d49af6c02b0
cayo <cayopf@proton.me>
2026-01-16 20:54:10 -0300

    atualizando carreiras_alura.ipynb

--- src/functions.py
+++ src/functions.py
@@ -3,7 +3,16 @@ from urllib.request import urlopen, Request
 
 
 def get_source(url):
-    headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+    #headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://www.google.com/',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+    }
     req = Request(url=url, headers=headers)
     html = urlopen(req).read()
     return html.decode()
